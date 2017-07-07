9366666e42b756e339927485d6dbc1a2c5cb0b4c
hawkeye116477 <hawkeye116477@gmail.com>
2017-07-07 13:29:08 +0200

    Remove 100% e10s turn on by default and cleaning

--- BUILD/build_wf-kde.sh
+++ BUILD/build_wf-kde.sh
@@ -3,6 +3,7 @@
 # Set current directory to script directory.
 Dir=$(cd "$(dirname "$0")" && pwd)
 cd $Dir
+
 # Init vars
 VERSION=""
 
@@ -33,32 +34,32 @@ else
 fi
 
 # Generate template directories
-if [ ! -d "$Dir/tmp/waterfox-$VERSION" ]; then
-    mkdir $Dir/tmp/waterfox-$VERSION
+if [ ! -d "$Dir/tmp/waterfox-kde-$VERSION" ]; then
+    mkdir $Dir/tmp/waterfox-kde-$VERSION
 fi
 
 # Copy deb templates
-if [ -d "$Dir/wf/debian" ]; then
-	cp -r $Dir/wf/debian/ $Dir/tmp/waterfox-$VERSION/
+if [ -d "$Dir/waterfox-kde/debian" ]; then
+	cp -r $Dir/waterfox-kde/debian/ $Dir/tmp/waterfox-kde-$VERSION/
 else
     echo "Unable to locate deb templates!"
     exit 1 
 fi
 
 # Copy latest build
-	cd $Dir/tmp/waterfox-$VERSION
-	wget https://storage-waterfox.netdna-ssl.com/releases/linux64/installer/waterfox-$VERSION.en-US.linux-x86_64.tar.bz2
-	tar jxf waterfox-$VERSION.en-US.linux-x86_64.tar.bz2
-	if [ -d "$Dir/tmp/waterfox-$VERSION/waterfox" ]; then
-	mv $Dir/tmp/waterfox-$VERSION/waterfox/browser/features/ $Dir/tmp/waterfox-$VERSION
+	cd $Dir/tmp/waterfox-kde-$VERSION
+    cp -r ~/git/Waterfox/objdir/dist/waterfox/ $Dir/tmp/waterfox-kde-$VERSION
+	if [ -d "$Dir/tmp/waterfox-kde-$VERSION/waterfox" ]; then
+	mv $Dir/tmp/waterfox-kde-$VERSION/waterfox/browser/features $Dir/tmp/waterfox-kde-$VERSION
 else
     echo "Unable to Waterfox package files, Please check the build was created and packaged successfully!"
     exit 1     
 fi
 
 # Generate change log template
-CHANGELOGDIR=$Dir/tmp/waterfox-$VERSION/debian/changelog
-if grep -q -E "__TIMESTAMP__" "$CHANGELOGDIR" ; then
+CHANGELOGDIR=$Dir/tmp/waterfox-kde-$VERSION/debian/changelog
+if grep -q -E "__VERSION__|__TIMESTAMP__" "$CHANGELOGDIR" ; then
+    sed -i "s|__VERSION__|$VERSION|" "$CHANGELOGDIR"
     DATE=$(date --rfc-2822)
     sed -i "s|__TIMESTAMP__|$DATE|" "$CHANGELOGDIR"
 else
@@ -67,24 +68,21 @@ else
 fi
 
 # Make sure correct permissions are set
-chmod  755 $Dir/tmp/waterfox-$VERSION/debian/waterfox.prerm
-chmod  755 $Dir/tmp/waterfox-$VERSION/debian/waterfox.postinst
-chmod 755 $Dir/tmp/waterfox-$VERSION/debian/rules
-chmod 755 $Dir/tmp/waterfox-$VERSION/debian/wrapper/waterfox
+chmod 755 $Dir/tmp/waterfox-kde-$VERSION/debian/waterfox-kde.prerm
+chmod 755 $Dir/tmp/waterfox-kde-$VERSION/debian/waterfox-kde.postinst
+chmod 755 $Dir/tmp/waterfox-kde-$VERSION/debian/rules
+chmod 755 $Dir/tmp/waterfox-kde-$VERSION/debian/wrapper/waterfox
 
 # Remove unnecessary files
-rm -rf $Dir/tmp/waterfox-$VERSION/waterfox/dictionaries
-rm -rf $Dir/tmp/waterfox-$VERSION/waterfox/updater
-rm -rf $Dir/tmp/waterfox-$VERSION/waterfox/updater.ini
-rm -rf $Dir/tmp/waterfox-$VERSION/waterfox/update-settings.ini
-rm -rf $Dir/tmp/waterfox-$VERSION/waterfox/precomplete
-rm -rf $Dir/tmp/waterfox-$VERSION/waterfox/removed-files
+rm -rf $Dir/tmp/waterfox-kde-$VERSION/waterfox/dictionaries
+rm -rf $Dir/tmp/waterfox-kde-$VERSION/waterfox/precomplete
+rm -rf $Dir/tmp/waterfox-kde-$VERSION/waterfox/removed-files
 
 # Build .deb package (Requires devscripts to be installed sudo apt install devscripts). Arch and based Linux needs -d flag.
 notify-send "Building deb packages!"
 debuild -us -uc -d
 
-if [ -f $Dir/tmp/waterfox_*_amd64.deb ]; then
+if [ -f $Dir/tmp/waterfox-kde_*_amd64.deb ]; then
     mv $Dir/tmp/*.deb $Dir/debs
 else
     echo "Unable to move deb packages the file maybe missing or had errors during creation!"
