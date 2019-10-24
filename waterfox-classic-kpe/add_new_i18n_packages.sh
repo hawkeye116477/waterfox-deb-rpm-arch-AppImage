27686c48f300c6088f940be6ed1fb11abc0617f8
hawkeye116477 <hawkeye116477@gmail.com>
2019-10-24 22:45:18 +0200

    It's time to combine waterfox & waterfox-kde
    Reason: KDE patches doesn't break package for other DE and most KDE users wants KDE patches and also without kwaterfoxhelper that features doesn't work. I also doesn't want to deal with four packages.

---
+++ waterfox-classic-kpe/add_new_i18n_packages.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+SCRIPT_PATH=$(dirname "$0")
+
+sed -i '/Package: waterfox-classic-i18n/Q' $SCRIPT_PATH/control
+mapfile -t _languages < <(cat $SCRIPT_PATH/locales.shipped)
+
+for _lang in "${_languages[@]}"; do
+  _locale_file=$(echo $_lang | awk -F: '{print $1}')
+  _locale_pkg=$(echo $_lang | awk -F: '{print tolower($1)}')
+  _locale_desc=$(echo $_lang | awk -F: '{print $2}')
+  _pkgname=waterfox-classic-i18n-$_locale_pkg
+  _pkgname_trans=waterfox-locale-$_locale_pkg
+    cat <<EOT >> $SCRIPT_PATH/control
+Package: $_pkgname
+Architecture: any
+Depends: \${misc:Depends}, waterfox-classic (>= \${source:Version})
+Replaces: $_pkgname_trans
+Breaks: $_pkgname_trans
+Description: $_locale_desc language pack for Waterfox Classic
+ This package contains $_locale_desc translations for Waterfox Classic.
+
+EOT
+done
+
+for _lang in "${_languages[@]}"; do
+  _locale_file=$(echo $_lang | awk -F: '{print $1}')
+  _locale_pkg=$(echo $_lang | awk -F: '{print tolower($1)}')
+  _locale_desc=$(echo $_lang | awk -F: '{print $2}')
+  _pkgname=waterfox-classic-i18n-$_locale_pkg
+  _pkgname_trans=waterfox-locale-$_locale_pkg
+    cat <<EOT >> $SCRIPT_PATH/control
+Package: $_pkgname_trans
+Architecture: any
+Section: oldlibs
+Depends: \${misc:Depends}, $_pkgname
+Description: Transitional package
+ This is a transitional package. It can safely be removed.
+
+EOT
+done
+
+touch $SCRIPT_PATH/${_pkgname}.install
+
+    cat <<EOT > $SCRIPT_PATH/${_pkgname}.install
+features/langpack-${_locale_file}@waterfox.xpi /usr/lib/waterfox/browser/features
+EOT
+
+mapfile -t trans_languages < <(cat $SCRIPT_PATH/locales.transitional)
+
+for trans_lang in "${trans_languages[@]}"; do
+  trans_pkg=$(echo $trans_lang | awk -F: '{print $1}' | sed 's/^/waterfox-locale-/')
+  depends=$(echo $trans_lang | awk -F: '{print $2}' | sed 's/^/waterfox-classic-i18n-/' | sed 's/,/, waterfox-classic-i18n-/g')
+
+    cat <<EOT >> $SCRIPT_PATH/control
+Package: $trans_pkg
+Architecture: any
+Section: oldlibs
+Depends: \${misc:Depends}, ${depends}
+Description: Transitional package
+ This is a transitional package. It can safely be removed.
+
+EOT
+done
+sed -i '$ d' $SCRIPT_PATH/control
