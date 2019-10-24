faf33505f5c4ca1ac4cdc7d98473f82f5e48e575
hawkeye116477 <hawkeye116477@gmail.com>
2019-10-24 17:20:03 +0200

    Fix for Current KPE

--- waterfox-current-kpe/add_new_i18n_packages.sh
+++ waterfox-current-kpe/add_new_i18n_packages.sh
@@ -7,15 +7,16 @@ mapfile -t _languages < <(cat $SCRIPT_PATH/locales.shipped)
 
 for _lang in "${_languages[@]}"; do
   _locale_file=$(echo $_lang | awk -F: '{print $1}')
+  _locale_pkg=$(echo $_lang | awk -F: '{print tolower($1)}')
   _locale_desc=$(echo $_lang | awk -F: '{print $2}')
-  _pkgname=waterfox-current-i18n-$_locale_file
+  _pkgname=waterfox-current-i18n-$_locale_pkg
     cat <<EOT >> $SCRIPT_PATH/control
-
 Package: $_pkgname
 Architecture: any
 Depends: \${misc:Depends}, waterfox-current (>= \${source:Version})
 Description: $_locale_desc language pack for Waterfox Current
  This package contains $_locale_desc translations for Waterfox Current
+
 EOT
 touch $SCRIPT_PATH/${_pkgname}.install
 
