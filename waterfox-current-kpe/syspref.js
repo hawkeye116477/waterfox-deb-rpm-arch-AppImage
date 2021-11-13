7fa552c77eb99621dcef0a809f3986177ffb39ef
hawkeye116477 <hawkeye116477@gmail.com>
2021-11-13 18:33:48 +0100

    Cleanup

--- waterfox-current-kpe/syspref.js
+++
@@ -1,3 +0,0 @@
-// This file can be used to configure global preferences for Waterfox Current
-// Example: Homepage
-//pref("browser.startup.homepage", "http://www.weebls-stuff.com/wab/");
--- waterfox-current-kpe/waterfox-current-wayland-bin.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# Run Waterfox Current under Wayland
-#
-export MOZ_ENABLE_WAYLAND=1
-exec /usr/lib/waterfox-current/waterfox-current "$@"
