31939451033dfd6b6a21908e839b9360e80a2352
hawkeye116477 <hawkeye116477@gmail.com>
2022-11-12 13:17:38 +0100

    [G] Remove transitional packages

--- waterfox-g-kpe/add_new_i18n_packages.sh
+++ waterfox-g-kpe/add_new_i18n_packages.sh
@@ -21,20 +21,6 @@ Breaks: $_pkgname_trans4 (<< 4.1.1-0~), $_pkgname_trans3 (<< 4.1.1-0~)
 Description: $_locale_desc language pack for Waterfox G
  This package contains $_locale_desc translations for Waterfox G
 
-Package: $_pkgname_trans4
-Architecture: all
-Section: oldlibs
-Depends: \${misc:Depends}, $_pkgname
-Description: Transitional package
- This is a transitional package. It can safely be removed.
-
-Package: $_pkgname_trans3
-Architecture: all
-Section: oldlibs
-Depends: \${misc:Depends}, $_pkgname
-Description: Transitional package
- This is a transitional package. It can safely be removed.
-
 EOT
 touch $SCRIPT_PATH/${_pkgname}.install
 
