a15a5e8d8c453a35b54011169e716d424597d0e5
hawkeye116477 <hawkeye116477@gmail.com>
2025-04-03 17:00:16 +0200

    Update script for making tarball

--- make_tarball.sh
+++ make_tarball.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
 APPNAME=$1
-VERSION=$2
+VERSION=$(git describe --tags --exact-match)
 cd ../Waterfox
 git ls-files --recurse-submodules | tar caf ../$APPNAME-$VERSION.tar.gz --verbatim-files-from -T- --xform s:^:$APPNAME-$VERSION/:
+
+obs_folder="waterfox-kde"
+if [[ "APPNAME" == "waterfox-g" ]]; then
+    obs_folder="waterfox-g-appimage"
+fi
+
+rm -rf  ~/obs/home:hawkeye116477:waterfox/${obs_folder}/tar_stamps
+
+cat << EOF >> ~/obs/home:hawkeye116477:waterfox/${obs_folder}/tar_stamps
+version: $VERSION
+commit: $(git rev-parse HEAD)
+EOF
